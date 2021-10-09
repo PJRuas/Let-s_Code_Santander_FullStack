5dd111f3969fc29c2f8367b620f3a18f0a7770b1
Pedro Ruas <pjruas123@gmail.com>
2021-10-08 22:18:39 -0300

    Update Module_2 Calculator
    Refactored Code for Operation Handler

--- module_02/module_calculator.js
+++ module_02/module_calculator.js
@@ -5,43 +5,34 @@ let calculatorModule = (function() {
     const checkOperation = () => { console.log(_calcCache.join(' '))};    
     const checkHistory = () => { console.log(_calcHistory)};    
     const enter = input => {_calcCache.push(input)};
-    const clearOperation = () => {_calcCache.length = 0 };
+    const clearOperation = (doNotUseLog) => {_calcCache.length = 0; if(!doNotUseLog){ console.log('Operation Cleared') } };
     const clearHistory = () => {_calcHistory.clear();
                                return console.log('History Cleared') };
     const reset = () =>  {clearOperation(); clearHistory()};
     const undo = () => { _calcCache.pop(); return};
 
-    const _operationHandler = (symbol, a, b) => {
-            switch(symbol){
-                case '+':
-                    return a + b;
-                case '-':
-                    return a - b;
-                case '/':
-                    return a / b;
-                case '*':
-                    return a * b;
-                case '%':
-                    return a % b;
-                case '**':
-                    return a ** b; 
-                default:
-                    console.log(`Operation "${symbol}" not recognized.`)
-            }
-    };    
+    const _sum = (x, y) =>  x + y;
+    const _subtract = (x, y) =>  x - y;
+    const _multiply = (x, y) => x * y;
+    const _divide = (x, y) => x / y;
+    const _floor = (x, y) => x % y;
+
+    const _operators = {'+':_sum, '-':_subtract, '*':_multiply, '/':_divide, '%':_floor};
+
+    const _operationHandler = (symbol, x, y) =>  _operators[symbol] ? _operators[symbol](x,y) : console.log(`Operation "${symbol}" not recognized.`);
 
     const equals = () => {
             let result = _operationHandler(_calcCache[1], _calcCache[0], _calcCache[2]);            
-            
+       
             for(let iten = 3; iten < _calcCache.length; iten++){
                 if(typeof _calcCache[iten] == 'string'){
                     result = _operationHandler(_calcCache[iten], result, _calcCache[iten+1])
                 }
             }
 
-            _calcHistory.set(_calcCache.join(' '), result)
+            _calcHistory.set(_calcCache.join(' '), result);
 
-            clearOperation()            
+            clearOperation(doNotUseLog = true);            
 
             return result
     };
@@ -55,7 +46,6 @@ let calculatorModule = (function() {
         clearHistory,
         reset,
         undo,
-        
     }
 
 }())
